39765ec0c9dda75981d283bb42f200e7f7532a79
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-09-22 10:05:55 +0200

    Thumbnails for next / pre post nav

--- single.php
+++ single.php
@@ -24,11 +24,34 @@
                     echo '</div>';
                 }
 
-                 if (strlen(get_next_post()->post_title) > 0 || strlen(get_previous_post()->post_title) > 0) {
+                // https://gist.github.com/nickdavis/862f79ca91be6a854243
+
+                $previousPost = get_adjacent_post( false, '', true );
+                $nextPost = get_adjacent_post( false, '', false );
+
+                 if ($previousPost || $nextPost) {
+
                      echo '<div class="l-clearfix m-dual-nav">';
                         echo '<h3 class="a-medium">Fler inlägg:</h3>';
-                        previous_post_link('%link');
-                        next_post_link('%link');
+
+                        if($previousPost){
+                            $prevPostfeatImg = get_the_post_thumbnail($previousPost->ID);
+
+                            if (function_exists('makeitSrcset') && $prevPostfeatImg) {
+                                makeitSrcset(get_post_thumbnail_id($previousPost->ID));
+                            }
+                            previous_post_link('%link');
+                        }
+
+                     if ($nextPost){
+                            $nextPostfeatImg = get_the_post_thumbnail($nextPost->ID);
+
+                         next_post_link('%link');
+                            if (function_exists('makeitSrcset') && $nextPostfeatImg) {
+                                makeitSrcset(get_post_thumbnail_id($nextPost->ID));
+                            }
+                        }
+
                      echo '</div>';
                  } ?>
             </aside>

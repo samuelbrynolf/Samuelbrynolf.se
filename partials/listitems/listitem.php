6c8f250f09593b8af0e0bd96ff5e06473241f198
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-09-30 11:20:29 +0200

    List items update

--- partials/listitems/listitem.php
+++ partials/listitems/listitem.php
@@ -1,16 +1,14 @@
-<a class="m-listitem__a" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+<a class="m-listitem__a<?php if(has_post_thumbnail()){ echo ' has-thumbnail'; } else { echo ' has-no-thumbnail'; } // SNYGGA TILL!!! ?>" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
     <article id="post-<?php the_ID(); ?>" <?php post_class('m-listitem'); ?>>
-        <header class="l-gutter m-listitem__header">
-            <h2 class="a-large a-listitem-title"><?php the_title(); ?></h2>
-        </header><!-- .listitem-header -->
-
-        <p class="a-fineprint a-listitem-meta-date"><?php the_date(); ?></p>
-
         <?php if ( function_exists('makeitSrcset') && has_post_thumbnail()) {
             makeitSrcset(get_post_thumbnail_id($post->ID), null, null, null, null, null, 'm-prf ratio-16-9');
         } elseif (has_post_thumbnail()){
             the_post_thumbnail();
         } ?>
+        <header class="l-gutter m-listitem__header">
+            <h2 class="a-large a-listitem-title"><?php the_title(); ?></h2>
+        </header><!-- .listitem-header -->
 
+        <p class="a-fineprint a-listitem-meta-date"><?php the_date(); ?></p>
     </article><!-- #post-## -->
 </a>

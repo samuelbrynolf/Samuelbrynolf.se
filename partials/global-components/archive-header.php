e55ebc98eb2c49973a326ab517eb7b5d97c38a3c
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-10-06 13:41:30 +0200

    Archive header styling

--- partials/global-components/archive-header.php
+++ partials/global-components/archive-header.php
@@ -1,11 +1,11 @@
-<header class="l-gutter m-archive__header a-icon arrowdown">
+<header class="l-gutter l-clearfix m-archive__header">
     <?php if(is_search()){ ?>
         <h1 class="a-xlarge">S&ouml;kresultat f&ouml;r <?php echo '&#145;'; the_search_query();echo '&#146;'; ?> <span>&mdash; <?php echo $wp_query->found_posts ?> tr&auml;ffar</span></h1>
     <?php } elseif(is_404()) { ?>
         <h1 class="a-xlarge"><span>404</span> Sidan existerar inte.</h1>
         <p class="a-fineprint">Tyvärr har sidan upphört, eller så är länken felaktig. <a href="<?php bloginfo('url'); ?>" title="Till startsidan">Gå till startsidan.</a></p>
     <?php } else {
-        the_archive_title( '<h1 class="a-xlarge">', '</h1>' );
-        the_archive_description( '<div class="a-fineprint">', '</div>' );
+        the_archive_title( '<h1>', '</h1>' );
+        the_archive_description('<div class="a-fineprint">','</div>');
     } ?>
 </header>

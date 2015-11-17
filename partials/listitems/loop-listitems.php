8ae630d937b12dbfbcf779f7174ff66b40681c82
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-11-17 11:01:46 +0100

    Styling: list item im-arrows + title margin for listen section

--- partials/listitems/loop-listitems.php
+++ partials/listitems/loop-listitems.php
@@ -1,8 +1,8 @@
-<?php echo '<div id="js-listitems">';
+<?php echo '<section id="js-listitems" class="o-listitems">';
     while ( have_posts() ) {
         the_post();
         get_template_part('partials/listitems/listitem', get_post_format());
     }
-echo '</div>';
+echo '</section>';
 
 next_posts_link('Visa fler inlägg');

a05fb60d5553331307f9be556275c960a339b621
Samuel Brynolf <samuel.brynolf@aventyret.com>
2015-11-18 08:09:06 +0100

    Lists vertical gutter fix, date-fix

--- partials/listitems/listitem.php
+++ partials/listitems/listitem.php
@@ -5,7 +5,7 @@
             <h2 class="a-large a-listitem-title"><?php the_title(); ?></h2>
         </header><!-- .listitem-header -->
 
-        <p class="a-fineprint a-listitem-meta-date"><?php the_date(); ?></p>
+        <p class="a-fineprint a-listitem-meta-date"><?php the_time('Y-m-d'); ?></p>
 
         <?php if ( function_exists('makeitSrcset') && has_post_thumbnail()) {
             makeitSrcset(get_post_thumbnail_id($post->ID), null, null, null, null, null, 'm-prf');

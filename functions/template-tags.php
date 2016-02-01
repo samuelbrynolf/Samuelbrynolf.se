c4e33b65320ba4af8a6ca526b595b6c6cd8de262
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-02-01 20:46:40 +0100

    Bug fix, additional divider

--- functions/template-tags.php
+++ functions/template-tags.php
@@ -7,10 +7,10 @@
             setup_postdata($post);
 
             echo '<a class="gallery-cell m-prf ratio-4-3 overlay" href="'.get_the_permalink().'" title="'.get_the_title().'">';
-            if ( function_exists('makeitSrcset') && has_post_thumbnail()) {
-                makeitSrcset(get_post_thumbnail_id($post->ID));
-            }
-            echo '<h3 class="a-medium a-prf-text">'.get_the_title().'</h3>';
+                if ( function_exists('makeitSrcset') && has_post_thumbnail()) {
+                    makeitSrcset(get_post_thumbnail_id($post->ID));
+                }
+                echo '<h3 class="a-medium a-prf-text">'.get_the_title().'</h3>';
             echo '</a>';
         } ?>
 

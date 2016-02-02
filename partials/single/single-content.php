96784be2d17344c8300b41b7e7eb55ce4109d835
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-02-02 12:36:36 +0100

    Post footer

--- partials/single/single-content.php
+++ partials/single/single-content.php
@@ -1,4 +1,10 @@
-<div id="js-entry-content" class="l-gutter o-entry-content">
-    <?php the_content(); ?>
-    <div class="l-clear"></div>
+<div id="js-entry-content" class="l-container o-entry-content">
+
+    <div class="l-span-A12 l-span-D7">
+        <?php the_content(); ?>
+        <div class="l-clear"></div>
+    </div>
+
+    <?php get_template_part('partials/single/single-footer'); ?>
+
 </div><!-- .entry-content -->
--- partials/single/single-footer.php
+++ partials/single/single-footer.php
@@ -1,3 +1,5 @@
-<footer class="l-gutter m-entry__footer">
-    <?php the_date('', '<p class="a-fineprint entry-footer">', '</p>'); ?>
-</footer><!-- .entry-footer -->
+<?php if(function_exists('get_field') && get_field('postfooter_resources')) { ?>
+    <footer class="l-span-A12 l-pre-D1 l-span-D4 m-resources">
+        <?php echo get_field('postfooter_resources'); ?>
+    </footer>
+<?php } ?>

a2e01eb6523cb229ee145e39db79997adb44351c
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-02-04 21:54:35 +0100

    Beta screen

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -219,28 +219,8 @@
     show_contact();
     top_tags('.js-toptags a');
 	
-	// -------------------------------------------------------------------------------------------------------
-	
-	
-	setTimeout(function(){
- 		var wflObjects = [
-	 		
-	 		'#wfl-test',
-	 		'.example-2'
- 		
- 		].join(', ');
-
-		$(wflObjects).css({
-			'visibility' 	: 'visible',
-			'opacity' 		: '1',
-			'transform'		: 'translate3d(0,0,0)'
-		});
-	},2500);
-	
 	
 	// -------------------------------------------------------------------------------------------------------
-
-
 
 
     $('#js-topjump').bind('tap', function(){

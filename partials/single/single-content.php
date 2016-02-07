d3aa9c1656f308fdf02bdfdd83c5dc79f8d2b4e9
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-02-07 22:59:50 +0100

    Preamble, block quote, lists,

--- partials/single/single-content.php
+++ partials/single/single-content.php
@@ -1,7 +1,8 @@
 <div id="js-entry-content" class="l-container o-entry-content">
 
     <div class="l-span-A12 l-span-D7">
-        <?php if (function_exists('get_field') && get_field('external_linkText') && get_field('external_linkUrl')){ ?>
+
+        <?php if (function_exists('get_field') && get_field('external_linkText') && get_field('external_linkUrl') && has_post_format('link', get_the_ID())){ ?>
             <p class="a-external__a">
                 Direktl&auml;nk: <a class="js-tappy" href="<?php echo get_field('external_linkUrl'); ?>"><?php echo get_field('external_linkText'); ?></a>
             </p>

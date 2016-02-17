7c963dda0043edd92bf5a79099add8f0da7d7382
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-02-17 20:46:23 +0100

    Adding tags to listitem

--- partials/listitems/listitem.php
+++ partials/listitems/listitem.php
@@ -1,6 +1,13 @@
 <a class="js-tappy m-listitem__a" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
     <?php the_title( '<h2 class="a-medium a-listitem-title">', '</h2>' ); ?>
-    <p class="a-fineprint a-listitem-excerpt"><span class="a-listitem-meta-date"><?php the_time('Y-m-d'); ?></span> &#183; <?php echo get_the_excerpt(); ?></p>
+    <p class="a-fineprint a-listitem-excerpt">
+        <span class="a-listitem-meta-date">
+            <?php the_time('Y-m-d');
+            if (has_tag()) {
+                echo strip_tags(get_the_tag_list(' &#183; ',' &#183; ',' '));
+            } ?>
+        </span>
+        &#183; <?php echo get_the_excerpt(); ?></p>
 </a>
 
 <?php if (function_exists('get_field') && get_field('external_linkText') && get_field('external_linkUrl')){ ?>

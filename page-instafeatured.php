611fe42b90213fa6d450c65692e0aaf5ab5ccb78
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-06-09 08:11:09 +0200

    Remove instagram load-container

--- page-instafeatured.php
+++ page-instafeatured.php
@@ -5,7 +5,7 @@ get_header(); ?>
         <?php while ( have_posts() ) : the_post(); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <?php get_template_part('partials/singular/singular-header');
-                echo '<div id="js-instagram" class="m-instagram"></div>';
+                //echo '<div id="js-instagram" class="m-instagram"></div>';
                 get_template_part('partials/singular/singular-content');
                 get_template_part('partials/singular/singular-aside');?>
             </article>
